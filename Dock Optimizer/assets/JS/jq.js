ff9d12f4e6da826abbe45c893561fea8372f2182
Vishnuprajapati <vishnu.prajapati@techcronus.com>
2023-04-20 17:48:42 +0530

    cards

--- Dock Optimizer/assets/JS/jq.js
+++ Dock Optimizer/assets/JS/jq.js
@@ -1,9 +1,3 @@
-$(document).ready(function () {
-  $('.launch-inner-modal').click(function () {
-    alert('The paragraph was clicked.');
-    // $('#myModel1').modal('hide');
-    setTimeout(() => {
-      $('.close-custom-btn').trigger('click');
-    }, 5000);
-  });
+$('#saveBtn').click(function () {
+  $('#demo-2').modal('hide');
 });
